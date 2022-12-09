60d15000938f9e9dcdb7cd7876418f78f2dced87
Александр Богус <nigromontan92@gmail.com>
2022-12-09 10:42:22 +0300

    collect static

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -104,13 +104,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = 'backend_static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static/')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = 'backend_media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media/')
 
 DATA_ROOT = os.path.join(BASE_DIR, 'data/')
 
