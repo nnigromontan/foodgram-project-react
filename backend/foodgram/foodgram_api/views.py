9d25cd570ba39d69aa965694bb4bb8756c1ac10c
Александр Богус <nigromontan92@gmail.com>
2022-12-13 17:51:54 +0300

    bug fixing v1.0

--- backend/foodgram/foodgram_api/views.py
+++ backend/foodgram/foodgram_api/views.py
@@ -28,7 +28,7 @@ class IngredientViewSet(
     serializer_class = IngredientSerializer
     permission_classes = (AllowAny,)
     filterset_class = IngredientFilter
-
+    pagination_class = None
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
@@ -106,3 +106,4 @@ class TagViewSet(
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = (AllowAny,)
+    pagination_class = None
