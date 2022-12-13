f5568e70a1027ea88bdb57b67a66543603cc8b21
Александр Богус <nigromontan92@gmail.com>
2022-12-13 20:31:25 +0300

    Tags fixing v1.4

--- backend/foodgram/foodgram_api/filters.py
+++ backend/foodgram/foodgram_api/filters.py
@@ -36,15 +36,12 @@ class RecipeFilter(filters.FilterSet):
             'is_in_shopping_cart',
         )
 
-    def get_favorite(self, queryset, name, value):
-        if value:
-            return queryset.filter(in_favorite__user=self.request.user)
-        return queryset.exclude(
-            in_favorite__user=self.request.user
-        )
+    def get_is_favorited(self, queryset, name, value):
+        if self.request.user.is_authenticated and value is True:
+            return queryset.filter(users_favorites__user=self.request.user)
+        return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
-        if value:
-            return Recipe.objects.filter(
-                shopping_recipe__user=self.request.user
-            )
+        if self.request.user.is_authenticated and value is True:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
