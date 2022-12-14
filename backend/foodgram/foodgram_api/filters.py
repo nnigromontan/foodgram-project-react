b2573b9fe3defa88f0decae512c651b7e002712f
Александр Богус <nigromontan92@gmail.com>
2022-12-14 15:35:16 +0300

    nginx fixing v1.4

--- backend/foodgram/foodgram_api/filters.py
+++ backend/foodgram/foodgram_api/filters.py
@@ -2,7 +2,8 @@
 
 from django_filters.rest_framework import FilterSet, filters
 
-from .models import Ingredient, Recipe
+from .models import Ingredient, Recipe,Tag
+from users.models import User
 
 
 class IngredientFilter(FilterSet):
@@ -13,28 +14,21 @@ class IngredientFilter(FilterSet):
         fields = ('name',)
 
 
-class RecipeFilter(FilterSet):
-    is_favorited = filters.BooleanFilter(
-        method='get_favorite',
-        label='favorite',
-    )
-    tags = filters.AllValuesMultipleFilter(
+class TagFilter(FilterSet):
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
-        label='tags',
+        queryset=Tag.objects.all(),
+        to_field_name='slug',
     )
+    is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        method='get_is_in_shopping_cart',
-        label='shopping_cart',
+        method='get_is_in_shopping_cart'
     )
 
     class Meta:
         model = Recipe
-        fields = (
-            'tags',
-            'author',
-            'is_favorited',
-            'is_in_shopping_cart',
-        )
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
     def get_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated and value is True:
