a4abdb4e62ca5230509b4aed1e8025d37ba9e0d0
Александр Богус <nigromontan92@gmail.com>
2022-12-15 12:04:03 +0300

    subscription fixes 1.5

--- backend/foodgram/foodgram_api/serializers.py
+++ backend/foodgram/foodgram_api/serializers.py
@@ -4,7 +4,7 @@ from django.db import transaction
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers, validators
 
-from users.serializers import CurrentUserSerializer
+from users.serializers import CustomUserSerializer
 from foodgram.settings import MIN
 from .models import (Favorite, Ingredient, IngredientsInRecipe, Recipe,
                      ShoppingCart, Tag)
@@ -53,7 +53,7 @@ class AddIngredientSerializer(serializers.ModelSerializer):
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    author = CurrentUserSerializer(read_only=True)
+    author = CustomUserSerializer(read_only=True)
     tags = TagSerializer(
         read_only=True,
         many=True,
