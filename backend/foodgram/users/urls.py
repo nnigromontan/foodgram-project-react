a919b53e507e1d9e852cc043a1a63e5e401b6931
Александр Богус <nigromontan92@gmail.com>
2022-12-15 11:29:55 +0300

    subscription fixes 1.5

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,11 +1,16 @@
 """Адреса приложения users проекта foodgram."""
 
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import SubscribeView, SubscriptionViewSet
+from .views import SubscribeView, SubscriptionViewSet, CustomUserViewSet
+
+router = DefaultRouter()
+router.register('users', CustomUserViewSet, basename='users')
 
 urlpatterns = [
     path('', include('djoser.urls')),
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
     path('users/subscriptions/',
          SubscriptionViewSet.as_view(),
