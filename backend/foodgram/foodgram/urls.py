795a3a7d16fa47e0f468dd551496fc139867cccc
Александр Богус <nigromontan92@gmail.com>
2022-12-16 10:22:03 +0300

    test

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -8,8 +8,9 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-#   path('api/', include('foodgram_api.urls')),
 ]
+#   path('api/', include('foodgram_api.urls')),
+
 
 if settings.DEBUG:
     urlpatterns = (
