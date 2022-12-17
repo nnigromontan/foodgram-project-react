2b5a24d79364dbd1fc73ee244f55aea2d42b25d2
Александр Богус <nigromontan92@gmail.com>
2022-12-17 21:24:15 +0300

    routers redefinition

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,25 +1,25 @@
 """Адреса приложения users проекта foodgram."""
 
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from users.views import SubscriptionViewSet
 
 app_name = 'users'
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(
     r'users/(?P<user_id>\d+)/subscribe',
     SubscriptionViewSet,
     basename='subscribe')
 router.register(
-    r'users/subscriptions',
+    r'users/subscriptions/',
     SubscriptionViewSet,
     basename='subscriptions')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('djoser.urls.authtoken')),
     path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
