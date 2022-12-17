fbfdb188bad5be893da83b92e9e2c22274bb899d
Александр Богус <nigromontan92@gmail.com>
2022-12-17 23:28:44 +0300

    urls bugfixing

--- backend/foodgram/foodgram_api/urls.py
+++ backend/foodgram/foodgram_api/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from foodgram_api.views import IngredientViewSet, RecipeViewSet, TagViewSet
-from users.views import SubscriptionViewSet
 
 app_name = 'foodgram_api'
 
@@ -12,15 +11,7 @@ router = DefaultRouter()
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('tags', TagViewSet, basename='tags')
-router.register(
-    r'users/(?P<user_id>\d+)/subscribe',
-    SubscriptionViewSet,
-    basename='subscribe')
-router.register(
-    r'users/subscriptions/',
-    SubscriptionViewSet,
-    basename='subscriptions')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,10 +1,25 @@
 """Адреса приложения users проекта foodgram."""
 
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from users.views import SubscriptionViewSet
 
 app_name = 'users'
 
+router = DefaultRouter()
+router.register(
+    r'users/(?P<user_id>\d+)/subscribe',
+    SubscriptionViewSet,
+    basename='subscribe')
+router.register(
+    r'users/subscriptions',
+    SubscriptionViewSet,
+    basename='subscriptions')
+
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
     path('', include('djoser.urls')),
 ]
