7d2e6cd9983acdef0eaa16dd39184ac574d4e464
Александр Богус <nigromontan92@gmail.com>
2022-12-17 22:42:32 +0300

    urls bugfixing

--- backend/foodgram/foodgram_api/filters.py
+++ backend/foodgram/foodgram_api/filters.py
@@ -1,24 +1,25 @@
 """Фильтры приложения foodgram_api."""
-from django_filters.rest_framework import FilterSet, filters
-from rest_framework import filters as restfilters
+
+from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework.filters import (
+    ModelChoiceFilter,
+    AllValuesMultipleFilter,
+    BooleanFilter)
+from rest_framework.filters import SearchFilter
 
 from .models import Recipe
 from users.models import User
 
 
-class IngredientSearchFilter(restfilters.SearchFilter):
+class IngredientSearchFilter(SearchFilter):
     search_param = 'name'
 
 
 class TagFilter(FilterSet):
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    tags = filters.AllValuesMultipleFilter(
-        field_name='tags__slug',
-    )
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='get_is_in_shopping_cart'
-    )
+    author = ModelChoiceFilter(queryset=User.objects.all())
+    tags = AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = BooleanFilter(method='get_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
