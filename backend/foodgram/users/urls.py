32c0328e267f41ee38df21114e4d5372ac1c87d8
Александр Богус <nigromontan92@gmail.com>
2022-12-17 12:23:40 +0300

    debug enable

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,11 +1,16 @@
 """Адреса приложения users проекта foodgram."""
 
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from users.views import SubscribeView, SubscriptionViewSet
+from users.views import SubscribeView, SubscriptionViewSet, UserViewSet
+
+router = DefaultRouter()
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include('djoser.urls')),
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
     path('users/subscriptions/', SubscriptionViewSet.as_view()),
     path('users/<int:pk>/subscribe/', SubscribeView.as_view()),
