59db9efc8ff4dcfcfe37ecbc1354ef05fc5950ea
Александр Богус <nigromontan92@gmail.com>
2022-12-17 21:43:48 +0300

    routers redefinition

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -6,11 +6,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('users.urls', namespace='users')),
-    path('api/', include('foodgram_api.urls', namespace='foodgram_api')),
-    path('api/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.authtoken')),
+    path(r'^admin/', admin.site.urls),
+    path(r'^api/', include('users.urls', namespace='users')),
+    path(r'^api/', include('foodgram_api.urls', namespace='foodgram_api')),
+    path(r'^api/', include('djoser.urls')),
+    path(r'^api/auth/', include('djoser.urls.authtoken')),
 ]
 
 if settings.DEBUG:
