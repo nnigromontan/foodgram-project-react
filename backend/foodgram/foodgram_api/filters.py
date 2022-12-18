4cffc0ab24ebefdebf031908d3e444a14bf8148b
Александр Богус <nigromontan92@gmail.com>
2022-12-18 13:03:52 +0300

    new filters params

--- backend/foodgram/foodgram_api/filters.py
+++ backend/foodgram/foodgram_api/filters.py
@@ -15,26 +15,6 @@ class IngredientSearchFilter(SearchFilter):
     search_param = 'name'
 
 
-class TagFilter(FilterSet):
-    author = ModelChoiceFilter(queryset=User.objects.all())
-    tags = AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = BooleanFilter(method='get_is_in_shopping_cart')
-
-    class Meta:
-        model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
-
-    def get_is_favorited(self, queryset, name, value):
-        if self.request.user.is_authenticated and value is True:
-            return queryset.filter(users_favorites__user=self.request.user)
-        return queryset
-
-    def get_is_in_shopping_cart(self, queryset, name, value):
-        if self.request.user.is_authenticated and value is True:
-            return queryset.filter(shopping_cart__user=self.request.user)
-        return queryset
-
 class RecipeFilter(FilterSet):
     author = ModelChoiceFilter(queryset=User.objects.all())
     tags = AllValuesMultipleFilter(field_name='tags__slug')
